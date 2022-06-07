97fc6f92a9b57d25f496ade4f14047dc35bed4e2
marcusloy77 <marcus.loy.77@gmail.com>
2022-06-07 15:59:40 +1000

    file restructure

--- graphics_card.js
+++ graphics_card.js
@@ -11,23 +11,12 @@ axios.get(url)
       // console.log(html)
       const $ = cheerio.load(html); 
       //console.log($('.goods_name').text())
-      let cardsArr = $('.goods_name').text().split("Graphics Card")
+      let cardsArr = $('.goods_name').text().split(" Graphics Card")
       let cardsImg = $('image/jpeg').text()
       
       cardsArr.pop()
       console.log(cardsArr)
       console.log(cardsImg)
-  }});
+}});
 
-// url = 'https://www.umart.com.au/pc-parts/computer-parts/graphics-cards-gpu-610';
-// axios.get(url)
-//   .then((response) => {
-//       if(response.status === 200) {
-//       const html = response.data;
-//       // console.log(html)
-//       const $ = cheerio.load(html); 
-//       //console.log($('.goods_name').text())
-//       let cardsArr = $('.goods_name').text().split("Graphics Card")
-//       cardsArr.pop()
-//       console.log(cardsArr)
-//   }});
+
