38df51020df851cd296a342669ab34b3ff0fef58
marcusloy77 <marcus.loy.77@gmail.com>
2022-06-07 15:17:48 +1000

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+  // const args = process.argv.slice(2);
+  // const postCode = args[0] || 2000;
+let url = 'https://www.umart.com.au/pc-parts/computer-parts/graphics-cards-gpu-610';
+axios.get(url)
+  .then((response) => {
+      if(response.status === 200) {
+      const html = response.data;
+      // console.log(html)
+      const $ = cheerio.load(html); 
+      //console.log($('.goods_name').text())
+      let cardsArr = $('.goods_name').text().split("Graphics Card")
+      let cardsImg = $('image/jpeg').text()
+      
+      cardsArr.pop()
+      console.log(cardsArr)
+      console.log(cardsImg)
+  }});
+
+// url = 'https://www.umart.com.au/pc-parts/computer-parts/graphics-cards-gpu-610';
+// axios.get(url)
+//   .then((response) => {
+//       if(response.status === 200) {
+//       const html = response.data;
+//       // console.log(html)
+//       const $ = cheerio.load(html); 
+//       //console.log($('.goods_name').text())
+//       let cardsArr = $('.goods_name').text().split("Graphics Card")
+//       cardsArr.pop()
+//       console.log(cardsArr)
+//   }});
